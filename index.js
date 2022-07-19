028b68ac0364575f2b29ac3d8ba30d238d092abf
theogom <theo.gom.2000@gmail.com>
2022-07-19 22:48:07 +0200

    Use minetest config file to generate the chunks map

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-let areas = [];
-min = -400;
-max = 400;
+const fs = require('fs');
 
-for(var x = min; x < max; x+=65)
-{
-    for(var z = min; z < max; z+=65)
-    {
-            areas.push({
-                "name": `Zone-${x}-${z}`,
-                "owner": "Ayrton",
-                "pos1": {
-                    "x": x,
-                    "y": -256,
-                    "z": z
-                },
-                "pos2": {
-                    "x": x+64,
-                    "y": 256,
-                    "z": z+64
-                }
-            })
+// Read minetest config file and get `mapgen_limit` value
+const config = fs.readFileSync('../minetest-server/minetest.conf', 'utf8');
+const mapgenLimit = config.match(/mapgen_limit\s*=\s*(\d+)/);
+
+if (!mapgenLimit) {
+    console.error('Could not find parameter mapgen_limit in minetest.conf');
+    process.exit(1);
+}
+
+// Generate chunks in a map of size `mapgen_limit` * `mapgen_limit`
+const areas = [];
+const min = -mapgenLimit[1];
+const max = mapgenLimit[1];
+
+for (let x = min; x < max; x += 65) {
+    for (let z = min; z < max; z += 65) {
+        areas.push({
+            "name": `Zone-${x}-${z}`,
+            "owner": "Ayrton",
+            "pos1": {
+                "x": x,
+                "y": -256,
+                "z": z
+            },
+            "pos2": {
+                "x": x + 64,
+                "y": 256,
+                "z": z + 64
+            }
+        });
     }
 }
-const fs = require('fs');
-fs.writeFileSync('../minetest-server/data/worlds/world/areas.dat', JSON.stringify(areas));
 
-console.log(areas);
+fs.writeFileSync('../minetest-server/data/worlds/world/areas.dat', JSON.stringify(areas));
