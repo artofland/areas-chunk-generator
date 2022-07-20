d1953e282971b5ce7c868469ce8cace1b3444c37
theogom <theo.gom.2000@gmail.com>
2022-07-20 09:00:09 +0200

    Configurable chunk size Avoid chunks overlaping Checking for only complete chunks

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 const fs = require('fs');
 
+const CHUNK_LENGTH = 64;
+const CHUNK_HEIGHT = 256; // Index of the highest block
+
 // Read minetest config file and get `mapgen_limit` value
 const config = fs.readFileSync('../minetest-server/minetest.conf', 'utf8');
-const mapgenLimit = config.match(/mapgen_limit\s*=\s*(\d+)/);
+const mapgenLimit = config.match(/mapgen_limit\s*=\s*(\d+)/)?.[1];
 
 if (!mapgenLimit) {
-    console.error('Could not find parameter mapgen_limit in minetest.conf');
+    console.error('Error: could not find parameter mapgen_limit in minetest.conf');
     process.exit(1);
 }
 
-// Generate chunks in a map of size `mapgen_limit` * `mapgen_limit`
-const areas = [];
-const min = -mapgenLimit[1];
-const max = mapgenLimit[1];
+if (mapgenLimit % CHUNK_LENGTH !== 0) {
+    console.error(`Error: mapgen_limit (${mapgenLimit}) is not a multiple of ${CHUNK_LENGTH}`);
+    process.exit(1);
+}
 
-for (let x = min; x < max; x += 65) {
-    for (let z = min; z < max; z += 65) {
+// Generate chunks of `CHUNK_LENGTH` * `CHUNK_LENGTH` * (`CHUNK_HEIGHT` * 2 + 1) blocks
+// in a map of size `mapgen_limit` * `mapgen_limit`
+const areas = [];
+const min = -mapgenLimit;
+const max = mapgenLimit;
+
+for (let x = min; x < max; x += CHUNK_LENGTH) {
+    for (let z = min; z < max; z += CHUNK_LENGTH) {
         areas.push({
-            "name": `Zone-${x}-${z}`,
-            "owner": "Ayrton",
-            "pos1": {
-                "x": x,
-                "y": -256,
-                "z": z
+            'name': `Zone-${x}-${z}`,
+            'owner': 'Ayrton',
+            'pos1': {
+                'x': x,
+                'y': -CHUNK_HEIGHT,
+                'z': z
             },
-            "pos2": {
-                "x": x + 64,
-                "y": 256,
-                "z": z + 64
+            'pos2': {
+                'x': x + CHUNK_LENGTH - 1,
+                'y': CHUNK_HEIGHT,
+                'z': z + CHUNK_LENGTH - 1
             }
         });
     }
 }
 
 fs.writeFileSync('../minetest-server/data/worlds/world/areas.dat', JSON.stringify(areas));
+
+console.log(`Generated ${areas.length} areas of size ${CHUNK_LENGTH} x ${CHUNK_LENGTH} in a ${mapgenLimit} x ${mapgenLimit} map`);
